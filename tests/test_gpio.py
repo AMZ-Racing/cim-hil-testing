220a025d5d8ede7c5376b76075119f7733d8e955
kaluri <kaluri@ethz.ch>
2026-05-13 11:58:22 +0200

    small fix

--- tests/test_gpio.py
+++ tests/test_gpio.py
@@ -1,3 +1,5 @@
+from unittest import result
+
 import RPi.GPIO as GPIO
 import pytest
 import time
@@ -67,9 +69,15 @@ def test_can_hat_gpio():
 
     #Bring up CAN interface
     # Example patch
-    subprocess.run(["sudo", "ip", "link", "set", "can0", "down"], check=False)
-    subprocess.run(["sudo", "ip", "link", "set", "can0", "up", "type", "can", "bitrate", "500000"], check=True)
+    #subprocess.run(["sudo", "ip", "link", "set", "can0", "down"], check=False)
+    #subprocess.run(["sudo", "ip", "link", "set", "can0", "up", "type", "can", "bitrate", "500000"], check=True)
+    result = subprocess.run(
+    ["ip", "link", "show", "can0"],
+    capture_output=True,
+    text=True
+    )
 
+    assert result.returncode == 0, "CAN interface can0 not found"
 
     #Open CAN bus
     can0 = can.Bus(
