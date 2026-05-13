f839e5b1e9b320b532c64e64122b2699f0e69ae8
kaluri <kaluri@ethz.ch>
2026-05-13 12:13:35 +0200

    even more fixes

--- can-scripts/[4] can_fd_test.py
+++ can-scripts/[4] can_fd_test.py
@@ -1,26 +1,23 @@
-# checks that CAN FD is available on CAN HAT
-import pytest
-import subprocess
 import can
-import time 
+import time
 
-# @pytest.mark.skip("Requires CAN interface setup")
+
 def test_can_fd_frame():
-    cbus = can.Bus(channel="vcan0", interface="socketcan")
+    bus = can.Bus(interface="socketcan", channel="vcan0")
 
-msg = can.Message(
-    arbitration_id=0x123,
-    data=[1, 2, 3, 4],
-    is_extended_id=False
-)
+    msg = can.Message(
+        arbitration_id=0x123,
+        data=[1, 2, 3, 4],
+        is_extended_id=False
+    )
 
-bus.send(msg)
+    bus.send(msg)
 
-time.sleep(0.05)
+    time.sleep(0.05)
 
-received = bus.recv(timeout=1.0)
+    received = bus.recv(timeout=1.0)
 
-bus.shutdown()
+    bus.shutdown()
 
-assert received is not None
-assert received.arbitration_id == 0x123
+    assert received is not None
+    assert received.arbitration_id == 0x123
--- can-scripts/[5] can_stress_test.py
+++ can-scripts/[5] can_stress_test.py
@@ -1,23 +1,23 @@
-import pytest
 import can
 import time
 
-# @pytest.mark.skip("Requires CAN interface setup")
+
 def test_can_stress():
-    bus = can.Bus(channel="vcan0", interface="socketcan")
+    bus = can.Bus(interface="socketcan", channel="vcan0")
 
-for i in range(100):
-    msg = can.Message(
-        arbitration_id=0x100 + i,
-        data=[i % 256],
-        is_extended_id=False
-    )
+    for i in range(100):
+        msg = can.Message(
+            arbitration_id=0x100 + i,
+            data=[i % 256],
+            is_extended_id=False
+        )
 
-    bus.send(msg)
+        bus.send(msg)
 
-    time.sleep(0.005)
+        time.sleep(0.005)
 
-    received = bus.recv(timeout=1.0)
-    assert received is not None
+        received = bus.recv(timeout=1.0)
 
-bus.shutdown()
+        assert received is not None
+
+    bus.shutdown()
